e80d737a59772d16376b0ea6b7de58813ee6caf1
Leslaures <lauremaffini@gmail.com>
2025-02-18 15:44:11 +0100

    Revert "S10 us07 qrcode student"

--- server/src/app.ts
+++ server/src/app.ts
@@ -51,9 +51,8 @@ app.use(
 // 4. `express.raw()`: Parses requests with raw binary data.
 
 // Uncomment one or more of these options depending on the format of the data sent by your client:
-import cookieParser from "cookie-parser";
-app.use(express.json());
-app.use(cookieParser());
+
+// app.use(express.json());
 // app.use(express.urlencoded());
 // app.use(express.text());
 // app.use(express.raw());
--- server/src/router.ts
+++ server/src/router.ts
@@ -2,106 +2,16 @@ import express from "express";
 
 const router = express.Router();
 
-import livreActions from "./modules/livre/livreActions";
-router.get("/api/:user_id/livres", livreActions.browse);
-router.get(
-  "/api/:user_id/livres_with_exemplaires",
-  livreActions.browseWithExemplaires,
-);
-router.get("/api/:user_id/livres/:ISBN13", livreActions.read);
-router.post("/api/:user_id/livres", livreActions.add);
-router.put("/api/:user_id/livres/:ISBN13", livreActions.edit);
-router.delete("/api/:user_id/livres/:ISBN13", livreActions.destroy);
-router.get("/api/:user_id/livres/search", livreActions.search);
-router.get("/api/:user_id/top_books", livreActions.getTopBooks);
+/* ************************************************************************* */
+// Define Your API Routes Here
+/* ************************************************************************* */
 
-import eleveActions from "./modules/eleve/eleveActions";
-router.get("/api/:user_id/eleves", eleveActions.browse);
-router.get("/api/:user_id/eleves/:id_eleve", eleveActions.read);
-router.get(
-  "/api/:user_id/eleves_with_borrows",
-  eleveActions.browseStudentsWithBorrowsInProgress,
-);
-router.get(
-  "/api/:user_id/eleves_with_borrows_information",
-  eleveActions.browseStudentsWithBorrowsInformation,
-);
-router.post("/api/:user_id/eleves", eleveActions.add);
-router.put("/api/:user_id/eleves/:id_eleve", eleveActions.edit);
-router.delete("/api/:user_id/eleves/:id_eleve", eleveActions.destroy);
-router.get("/api/:user_id/eleves/search", eleveActions.search);
+// Define item-related routes
+import itemActions from "./modules/item/itemActions";
 
-import exemplaireActions from "./modules/exemplaire/exemplaireActions";
-router.get("/api/:user_id/exemplaires", exemplaireActions.browse);
-router.get("/api/:user_id/exemplaires/:id_exemplaire", exemplaireActions.read);
-router.post("/api/:user_id/exemplaires", exemplaireActions.add);
-router.put("/api/:user_id/exemplaires/:id_exemplaire", exemplaireActions.edit);
-router.delete(
-  "/api/:user_id/exemplaires/:id_exemplaire",
-  exemplaireActions.destroy,
-);
-router.get(
-  "/api/:user_id/exemplaires_available",
-  exemplaireActions.readAvailableExemplaire,
-);
-router.get(
-  "/api/:user_id/exemplaires_borrowed/:ISBN13",
-  exemplaireActions.readBorrowedExemplaireByISBN13,
-);
-
-import empruntActions from "./modules/emprunt/empruntActions";
-router.get("/api/:user_id/emprunts", empruntActions.browse);
-router.get("/api/:user_id/emprunts/:id_emprunt", empruntActions.read);
-router.post("/api/:user_id/emprunts", empruntActions.addBookBorrow);
-router.put("/api/:user_id/emprunts/:id_emprunt", empruntActions.edit);
-router.delete("/api/:user_id/emprunts/:id_emprunt", empruntActions.destroy);
-router.get(
-  "/api/:user_id/students-with-loans-in-progress",
-  empruntActions.countStudentsWithLoansInProgress,
-);
-router.get(
-  "/api/:user_id/students-with-loans-due-in-7-days",
-  empruntActions.countStudentsWithLoansDueSoon,
-);
-router.get(
-  "/api/:user_id/students-with-overdue-loans",
-  empruntActions.countStudentsWithOverdueLoans,
-);
-router.get(
-  "/api/:user_id/emprunts_in-progress",
-  empruntActions.LoansInProgress,
-);
-router.get(
-  "/api/:user_id/emprunts_by_student/:id_eleve",
-  empruntActions.LoansByStudent,
-);
-
-import retour from "./modules/retour/retourActions";
-router.post("/api/:user_id/retours", retour.returnBook);
-
-import parametreActions from "./modules/parametre/parametreActions";
-router.get(
-  "/api/:user_id/parametres/loanDuration",
-  parametreActions.readLoanDuration,
-);
-router.put(
-  "/api/:user_id/parametres/loanDuration",
-  parametreActions.editLoanDuration,
-);
-router.get(
-  "/api/:user_id/parametres/borrowLimit",
-  parametreActions.readBorrowLimit,
-);
-router.put(
-  "/api/:user_id/parametres/borrowLimit",
-  parametreActions.editBorrowLimit,
-);
-
-import authActions from "./modules/auth/authActions";
-router.post("/api/auth/login", authActions.login);
-router.post("/api/auth/register", authActions.register);
-// router.post("/api/auth/logout", authActions.logout);
-// router.get("/api/auth/user", authActions.getUser);
+router.get("/api/items", itemActions.browse);
+router.get("/api/items/:id", itemActions.read);
+router.post("/api/items", itemActions.add);
 
 /* ************************************************************************* */
 
